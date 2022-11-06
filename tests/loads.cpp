95989c51f5c6a2eebb986deda13336495294a62c
Roberto Bertelli <robbybertelli@gmail.com>
2022-11-06 18:27:20 +0100

    Utilized macros to make test shorter and more readable
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- tests/loads.cpp
+++ tests/loads.cpp
@@ -5,74 +5,54 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("LDA IMMEDIATE", "[LDA]") {
-	INIT_TEST_ONCE(0xDE, 2)
+#define TEST_LDA(value, cycles, function, opcode, section_name) \
+	SECTION(section_name) { \
+		INIT_TEST \
+		function(cpu, opcode, &cpu.A, value, value, cycles); \
+		test_ZN_flags(cpu.A, flags, cpu.flags); \
+	}
 
-	test_immediate(cpu, LDA_IMMEDIATE, &cpu.A, value, value, cycles);
-	test_ZN_flags(cpu.A, flags, cpu.flags);
+TEST_CASE("LDA") {
+	TEST_LDA(0xDE, 2, test_immediate, LDA_IMMEDIATE, "LDA IMMEDIATE");
+	TEST_LDA(0xAF, 3, test_zero_page, LDA_ZERO_PAGE, "LDA ZERO PAGE");
+	TEST_LDA(0x0E, 4, test_zero_page_X, LDA_ZERO_PAGE_X, "LDA ZERO PAGE X");
+	TEST_LDA(0xFF, 4, test_absolute, LDA_ABSOLUTE, "LDA ABSOLUTE");
+	TEST_LDA(0x01, 4, test_absolute_X, LDA_ABSOLUTE_X, "LDA ABSOLUTE X");
+	TEST_LDA(0x04, 4, test_absolute_Y, LDA_ABSOLUTE_Y, "LDA ABSOLUTE Y");
+	TEST_LDA(0x07, 6, test_indirect_X, LDA_INDIRECT_X, "LDA INDIRECT X");
+	TEST_LDA(0x07, 5, test_indirect_Y, LDA_INDIRECT_Y, "LDA INDIRECT Y");
 }
 
-TEST_CASE("LDA ZERO PAGE", "[LDA]") {
-	INIT_TEST
-	test_zero_page(cpu, LDA_ZERO_PAGE, &cpu.A, 0xAF, 0xAF, 3);
-	test_zero_page_X(cpu, LDA_ZERO_PAGE_X, &cpu.A, 0xDE, 0xDE, 4);
-	test_ZN_flags(cpu.A, flags, cpu.flags);
+#define TEST_LDX(value, cycles, function, opcode, section_name) \
+	SECTION(section_name) { \
+		INIT_TEST \
+		function(cpu, opcode, &cpu.X, value, value, cycles); \
+		test_ZN_flags(cpu.X, flags, cpu.flags); \
+	}
+
+TEST_CASE("LDX") {
+	TEST_LDX(0xEE, 2, test_immediate, LDX_IMMEDIATE, "LDX IMMEDIATE");
+	TEST_LDX(0x44, 3, test_zero_page, LDX_ZERO_PAGE, "LDX ZERO PAGE");
+	TEST_LDX(0x64, 4, test_zero_page_Y, LDX_ZERO_PAGE_Y, "LDX ZERO PAGE Y");
+	TEST_LDX(0x12, 4, test_absolute, LDX_ABSOLUTE, "LDX ABSOLUTE");
+	TEST_LDX(0x18, 4, test_absolute_Y, LDX_ABSOLUTE_Y, "LDX ABSOLUTE Y");
 }
 
-TEST_CASE("LDA ABSOLUTE", "[LDA]") {
-	INIT_TEST
-	test_absolute(cpu, LDA_ABSOLUTE, &cpu.A, 0xFF, 0xFF, 4);
-	test_absolute_X(cpu, LDA_ABSOLUTE_X, &cpu.A, 0x01, 0x01, 4, true);
-	test_absolute_Y(cpu, LDA_ABSOLUTE_Y, &cpu.A, 0x04, 0x04, 4, true);
-	test_ZN_flags(cpu.A, flags, cpu.flags);
+#define TEST_LDY(value, cycles, function, opcode, section_name) \
+	SECTION(section_name) { \
+		INIT_TEST \
+		function(cpu, opcode, &cpu.Y, value, value, cycles); \
+		test_ZN_flags(cpu.Y, flags, cpu.flags); \
+	}
+
+TEST_CASE("LDY") {
+	TEST_LDY(0x99, 2, test_immediate, LDY_IMMEDIATE, "LDY IMMEDIATE");
+	TEST_LDY(0x73, 3, test_zero_page, LDY_ZERO_PAGE, "LDY ZERO PAGE");
+	TEST_LDY(0xEE, 4, test_zero_page_X, LDY_ZERO_PAGE_X, "LDY ZERO PAGE X");
+	TEST_LDY(0xAF, 4, test_absolute, LDY_ABSOLUTE, "LDY ABSOLUTE");
+	TEST_LDY(0xFF, 4, test_absolute_X, LDY_ABSOLUTE_X, "LDY ABSOLUTE X");
 }
 
-TEST_CASE("LDA INDIRECT", "[LDA]") {
-	INIT_TEST
-	test_indirect_X(cpu, LDA_INDIRECT_X, &cpu.A, 0x07, 0x07, 6);
-	test_indirect_Y(cpu, LDA_INDIRECT_Y, &cpu.A, 0x84, 0x84, 5);
-	test_ZN_flags(cpu.A, flags, cpu.flags);
-}
-
-TEST_CASE("LDX IMMEDIATE", "[LDX]") {
-	INIT_TEST
-	test_immediate(cpu, LDX_IMMEDIATE, &cpu.X, 0xEE, 0xEE, 2);
-	test_ZN_flags(cpu.X, flags, cpu.flags);
-}
-
-TEST_CASE("LDX ZERO PAGE", "[LDX]") {
-	INIT_TEST
-	test_zero_page(cpu, LDX_ZERO_PAGE, &cpu.X, 0x44, 0x44, 3);
-	test_zero_page_Y(cpu, LDX_ZERO_PAGE_Y, &cpu.X, 0x64, 0x64, 4);
-	test_ZN_flags(cpu.X, flags, cpu.flags);
-}
-
-TEST_CASE("LDX ABSOLUTE", "[LDX]") {
-	INIT_TEST
-	test_absolute(cpu, LDX_ABSOLUTE, &cpu.X, 0x12, 0x12, 4);
-	test_absolute_Y(cpu, LDX_ABSOLUTE_Y, &cpu.X, 0x18, 0x18, 4, true);
-	test_ZN_flags(cpu.X, flags, cpu.flags);
-}
-
-TEST_CASE("LDY IMMEDIATE", "[LDY]") {
-	INIT_TEST
-	test_immediate(cpu, LDY_IMMEDIATE, &cpu.Y, 0x99, 0x99, 2);
-	test_ZN_flags(cpu.Y, flags, cpu.flags);
-}
-
-TEST_CASE("LDY ZERO PAGE", "[LDY]") {
-	INIT_TEST
-	test_zero_page(cpu, LDY_ZERO_PAGE, &cpu.Y, 0x73, 0x73, 3);
-	test_zero_page_X(cpu, LDY_ZERO_PAGE_X, &cpu.Y, 0xEE, 0xEE, 4);
-	test_ZN_flags(cpu.Y, flags, cpu.flags);
-}
-
-TEST_CASE("LDY ABSOLUTE", "[LDY]") {
-	INIT_TEST
-	test_absolute(cpu, LDY_ABSOLUTE, &cpu.Y, 0xAF, 0xAF, 4);
-	test_absolute_X(cpu, LDY_ABSOLUTE_X, &cpu.Y, 0xFF, 0xFF, 4, true);
-	test_ZN_flags(cpu.Y, flags, cpu.flags);
-}
 // TEST_CASE("JSR", "[CPU]") {
 // 	cpu.reset(0xFF00);
 // 	mem[0xFF00] = JSR;
